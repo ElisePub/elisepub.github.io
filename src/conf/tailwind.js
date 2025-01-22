3c508188493c451b66efeb31978335b488df9c67
Elise <elise.pubert@gmail.com>
2025-01-22 21:41:28 +0100

    config tailwind et premiere vue

---
+++ src/conf/tailwind.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          'white' : '#efefef',
+          'dark' : '#0d1117',
+          'blue' : '#815bff',
+          'purple' : '#b55bff'
+        },
+        fontFamily: {
+          'title' : '',
+          'body' : ["HussarBold", "sans-serif"]
+        }
+      }
+    }
+  }
